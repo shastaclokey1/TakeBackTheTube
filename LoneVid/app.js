1a7085a54bcfc7ff568eee7811d3ad66631b5e2b
ShastaClokey <shasta.clokey@imdexlimited.com>
2020-12-06 11:03:48 -0800

    Added in a template project to start building lonevid

---
+++ LoneVid/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(request, response) {
+    response.render("landing");
+});
+
+app.get("/about", function(request, response) {
+    response.render("about");
+});
+
+app.get("/portfolio", function(request, response) {
+    response.render("portfolio");
+});
+
+app.get("/contact", function(request, response) {
+    response.render("contact");
+});
+
+app.get("*", function(request, response) {
+    response.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server has started for John's website");
+});
